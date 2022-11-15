c0d25e9b67ce12d4f638a83ff071cba330ad6412
SnehaEzhilan <101406544+SnehaEzhilan@users.noreply.github.com>
2022-11-15 19:57:41 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 3/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/getdata')
+def home():
+    return render_template('Home page.html')
+
+@app.route('/getdata', methods=['post'])
+def data():
+    name1 = request.form['username']
+    print(name1)
+    output = str(name1)
+    return render_template('Home page.html', text=output)
+
+
+if __name__ == '__main__':
+    app.run()
